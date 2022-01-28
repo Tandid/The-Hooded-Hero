f580b111cd3e078dbb4d50ed4b293d5bdeba2796
Tandid Alam <tandid.alam@gmail.com>
2022-01-27 19:36:10 -0500

    map loaded onto site

---
+++ src/scenes/Play.js
@@ -0,0 +1,17 @@
+import Phaser from "phaser";
+
+class Play extends Phaser.Scene {
+  constructor() {
+    super("PlayScene");
+  }
+
+  create() {
+    const map = this.make.tilemap({ key: "map" });
+    const tileset1 = map.addTilesetImage("main_lev_build_1", "tiles-1");
+
+    map.createStaticLayer("environment", tileset1);
+    map.createStaticLayer("platforms", tileset1);
+  }
+}
+
+export default Play;
