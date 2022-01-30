c12680df81ce5ae6e8c23e35413d51c030d233b4
Tandid Alam <tandid.alam@gmail.com>
2022-01-29 20:15:22 -0500

    added enemy sprite

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -1,5 +1,6 @@
 import Phaser from "phaser";
 import Player from "../entities/Player";
+import Birdman from "../entities/Birdman";
 
 class Play extends Phaser.Scene {
   constructor(config) {
@@ -12,6 +13,13 @@ class Play extends Phaser.Scene {
     const layers = this.createLayers(map);
     const playerZones = this.getPlayerZones(layers.playerZones);
     const player = this.createPlayer(playerZones.start);
+    const enemy = this.createEnemy();
+
+    this.createEnemyColliders(enemy, {
+      colliders: {
+        platformsColliders: layers.platformsColliders,
+      },
+    });
 
     this.createPlayerColliders(player, {
       colliders: {
@@ -46,6 +54,14 @@ class Play extends Phaser.Scene {
 
   createPlayer(start) {
     return new Player(this, start.x, start.y);
+  }
+
+  createEnemy() {
+    return new Birdman(this, 200, 200);
+  }
+
+  createEnemyColliders(enemy, { colliders }) {
+    enemy.addCollider(colliders.platformsColliders);
   }
 
   createPlayerColliders(player, { colliders }) {
