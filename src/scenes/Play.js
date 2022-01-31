6700c3b3019c944cc98c5b7ef5019f1dfe7b4b4a
Tandid Alam <tandid.alam@gmail.com>
2022-01-30 19:44:34 -0500

    attempt in adding lines for raycasting

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -31,6 +31,42 @@ class Play extends Phaser.Scene {
 
     this.createEndOfLevel(playerZones.end, player);
     this.setupFollowupCameraOn(player);
+
+    this.plotting = false;
+    this.graphics = this.add.graphics();
+    this.line = new Phaser.Geom.Line();
+    this.graphics.lineStyle(1, 0x00ff00);
+
+    this.input.on("pointerdown", this.startDrawing, this);
+    this.input.on(
+      "pointerup",
+      (pointer) => this.finishDrawing(pointer, layers.platforms),
+      this
+    );
+  }
+
+  startDrawing(pointer) {
+    this.line.x1 = pointer.worldX;
+    this.line.y1 = pointer.worldY;
+    this.plotting = true;
+  }
+
+  finishDrawing(pointer, layer) {
+    this.line.x2 = pointer.worldX;
+    this.line.y2 = pointer.worldY;
+
+    this.graphics.clear();
+    this.graphics.strokeLineShape(this.line);
+
+    this.tileHits = layer.getTilesWithinShape(this.line);
+
+    if (this.tileHits.length > 0) {
+      this.tileHits.forEach((tile) => {
+        tile.index !== -1 && console.log("I have hit the platform!");
+      });
+    }
+
+    this.plotting = false;
   }
 
   createMap() {
@@ -117,8 +153,19 @@ class Play extends Phaser.Scene {
 
     const eolOverlap = this.physics.add.overlap(player, endOfLevel, () => {
       eolOverlap.active = false;
-      console.log("Payer has won!");
+      console.log("Player has won!");
     });
+  }
+
+  update() {
+    if (this.plotting) {
+      const pointer = this.input.activePointer;
+
+      this.line.x2 = pointer.worldX;
+      this.line.y2 = pointer.worldY;
+      this.graphics.clear();
+      this.graphics.strokeLineShape(this.line);
+    }
   }
 }
 
