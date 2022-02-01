95707008ba0a27f10693889722d2153b1745eee2
Tandid Alam <tandid.alam@gmail.com>
2022-02-01 15:07:41 -0500

    projectile animation disappears after it hits

--- src/attacks/Projectile.js
+++ src/attacks/Projectile.js
@@ -1,5 +1,6 @@
 import Phaser from "phaser";
 import SpriteEffect from "../effects/SpriteEffect";
+import EffectManager from "../effects/EffectManager";
 
 class Projectile extends Phaser.Physics.Arcade.Sprite {
   constructor(scene, x, y, key) {
@@ -13,6 +14,8 @@ class Projectile extends Phaser.Physics.Arcade.Sprite {
     this.traveledDistance = 0;
     this.damage = 10;
     this.cooldown = 500;
+
+    this.effectManager = new EffectManager(this.scene);
   }
 
   preUpdate(time, delta) {
@@ -41,7 +44,7 @@ class Projectile extends Phaser.Physics.Arcade.Sprite {
     this.activateProjectile(false);
     this.traveledDistance = 0;
     this.body.reset(0, 0);
-    new SpriteEffect(this.scene, 0, 0, "hit-effect").playOn(target);
+    this.effectManager.playEffectOn("hit-effect", target);
   }
 
   activateProjectile(isActive) {
--- src/effects/SpriteEffect.js
+++ src/effects/SpriteEffect.js
@@ -9,10 +9,25 @@ class SpriteEffect extends Phaser.Physics.Arcade.Sprite {
 
     this.target = null;
     this.effectName = effectName;
+
+    this.on(
+      "animationcomplete",
+      (animation) => {
+        if (animation.key === this.effectName) {
+          this.destroy();
+        }
+      },
+      this
+    );
+  }
+
+  preUpdate(time, delta) {
+    super.preUpdate(time, delta);
+    this.placeEffect();
   }
 
   placeEffect() {
-    if (!this.target) {
+    if (!this.target || !this.body) {
       return;
     }
     const center = this.target.getCenter();
