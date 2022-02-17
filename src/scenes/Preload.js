1cecf70519b5c726e4e4b161728651382a8e90d3
Tandid Alam <tandid.alam@gmail.com>
2022-02-17 05:07:49 -0500

    minor fix

--- src/scenes/Preload.js
+++ src/scenes/Preload.js
@@ -471,11 +471,11 @@ class Preload extends Phaser.Scene {
 
   startGame() {
     this.registry.set("level", 1);
-    this.registry.set("level", 2);
-    this.registry.set("level", 3);
+    // this.registry.set("level", 2);
+    // this.registry.set("level", 3);
     this.registry.set("unlocked-levels", 1);
-    this.registry.set("unlocked-levels", 2);
-    this.registry.set("unlocked-levels", 3);
+    // this.registry.set("unlocked-levels", 2);
+    // this.registry.set("unlocked-levels", 3);
     this.scene.start("LoadingScene");
   }
 }
